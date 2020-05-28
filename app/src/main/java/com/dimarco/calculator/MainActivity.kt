d4aee1249f2d6701b1945f648cc42d4174bd56c4
Kevin <kevin.j.dimarco@gmail.com>
2020-05-28 15:53:18 -0700

    Added function to put numbers into txt

--- app/src/main/java/com/dimarco/calculator/MainActivity.kt
+++ app/src/main/java/com/dimarco/calculator/MainActivity.kt
@@ -2,11 +2,43 @@ package com.dimarco.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+
+    // all clickable button methods go here
+    fun numberClick(view: View) {
+        val numberSelected = view as Button
+        var number = ""
+        // finding which button was selected
+        when (numberSelected.id) {
+            button0.id -> number = "0"
+            button1.id -> number = "1"
+            button2.id -> number = "2"
+            button3.id -> number = "3"
+            button4.id -> number = "4"
+            button5.id -> number = "5"
+            button6.id -> number = "6"
+            button7.id -> number = "7"
+            button8.id -> number = "8"
+            button9.id -> number = "9"
+        }
+        lbl.append(number)
+    }
+
+
+
+    fun clickAC(view: View) {
+        lbl.text = "0"
     }
 }
